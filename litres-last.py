0123c357eabef2860a2b7716781e0574035fbe64
Denis Malinovsky <dmalinovsky@gmail.com>
2018-01-07 14:55:21 -0800

    Auto-update SID and show HTML with all download links

--- litres-last.py
+++ litres-last.py
@@ -2,6 +2,8 @@
 # encoding=utf-8
 """Gets the trial version of the most recent book in LitRes user basket."""
 
+from __future__ import unicode_literals
+
 import ConfigParser
 import getpass
 import gzip
@@ -10,10 +12,12 @@ import StringIO
 import subprocess
 import urllib
 import urllib2
+import webbrowser
 from xml.dom.minidom import parseString
 
 
 CONFIG = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'litres.ini')
+BASKET_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'basket.html')
 
 API_URL = 'http://robot.litres.ru/pages/'
 AUTHORIZE_URL = API_URL + 'catalit_authorise/'
@@ -24,6 +28,7 @@ AUTHORIZATION_OK_TAG = 'catalit-authorization-ok'
 AUTHORIZATION_FAILED_TAG = 'catalit-authorization-failed'
 BOOK_TAG = 'fb2-book'
 BOOK_TITLE_TAG = 'book-title'
+BOOK_AUTHOR_TAG = 'author'
 
 DOWNLOAD_DIR = os.path.expanduser('~/Downloads')
 
@@ -40,8 +45,11 @@ class LitresAPI(object):
     def login(self):
         sid_authorized = False
         if self.sid:
-            self.authorize_sid()
-            sid_authorized = True
+            try:
+                self.authorize_sid()
+                sid_authorized = True
+            except ValueError:
+                pass
         if sid_authorized:
             return
 
@@ -63,13 +71,46 @@ class LitresAPI(object):
         reply = self._read_xml(CATALOG_URL, {'basket': 1}, BOOK_TAG)
         return reply
 
+    def get_book_title(self, book):
+        return book.getElementsByTagName(BOOK_TITLE_TAG)[0].childNodes[0].data
+
+    def get_book_author(self, book):
+        author = book.getElementsByTagName(BOOK_AUTHOR_TAG)[0]
+        author.normalize()
+        name = '%s %s' % (
+            author.getElementsByTagName('first-name')[0].childNodes[0].data,
+            author.getElementsByTagName('last-name')[0].childNodes[0].data,
+        )
+        return name
+
     def download_trial(self, book):
         book_id = '%08d' % int(book.getAttribute('hub_id'))
         url = '%s%s/%s/%s/%s.epub' % (DOWNLOAD_TRIAL_URL, book_id[0:2],
                 book_id[2:4], book_id[4:6], book_id)
         subprocess.Popen(['wget', '-P', DOWNLOAD_DIR, url])
-        print 'Downloading "%s"' % \
-            book.getElementsByTagName(BOOK_TITLE_TAG)[0].childNodes[0].data
+        print 'Downloading "%s"' % self.get_book_title(book)
+
+    def get_book_url(self, book):
+        book_id = '%08d' % int(book.getAttribute('hub_id'))
+        url = '%s%s/%s/%s/%s' % (DOWNLOAD_TRIAL_URL, book_id[0:2],
+                book_id[2:4], book_id[4:6], book_id)
+        return url
+
+    def generate_html(self, books):
+        html = '<html><head><meta charset="utf-8"></head><body>'
+        for book in books:
+            url = self.get_book_url(book)
+            title = self.get_book_title(book)
+            author = self.get_book_author(book)
+            html += """
+                <p>%s. «%s» <a href="%s.fb2">FB2</a> | <a href="%s.epub">ePub</a></p>
+            """ % (author, title, url, url)
+
+        html += '</body></html>'
+        with open(BASKET_FILE, 'wb') as fp:
+            fp.write(html.encode('utf8'))
+
+        webbrowser.open_new_tab('file://%s' % BASKET_FILE)
 
     def _read_xml(self, url, data, desired_tag):
         """
@@ -92,7 +133,7 @@ class LitresAPI(object):
         if dom.firstChild.tagName == AUTHORIZATION_FAILED_TAG:
             self.sid = ''
             self._save_config()
-            raise Exception('Authorization failed, consider re-login')
+            raise ValueError('Authorization failed, consider re-login')
 
         return dom.getElementsByTagName(desired_tag)
 
@@ -114,5 +155,5 @@ class LitresAPI(object):
 if __name__ == '__main__':
     api = LitresAPI()
     api.login()
-    last_book = api.get_basket_items()[0]
-    api.download_trial(last_book)
+    basket = api.get_basket_items()
+    api.generate_html(basket)
